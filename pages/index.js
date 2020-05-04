d5e50ed6cfbff14dd6c13aa9c21d2aa6767cba20
Brian Chiang <chiangbri@gmail.com>
2020-05-03 18:09:20 -0700

    boardsize input successfully updating boardsize useState hook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 
 const App = () => {
-  return <div>Hello World</div>;
+  const [boardsize, setBoardsize] = useState(8);
+  const [boardsizeInput, setBoardsizeInput] = useState(0);
+
+  useEffect(() => {
+    console.log(boardsize);
+  });
+
+  function handleBoardsizeChange(e) {
+    e.preventDefault();
+    setBoardsize(boardsizeInput);
+  }
+
+  function handleBoardsizeInputChange(e) {
+    e.preventDefault();
+    setBoardsizeInput(e.target.value);
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Checkerboard</title>
+      </Head>
+      <form id='boardsizeForm' onSubmit={handleBoardsizeChange}>
+        <input
+          type='text'
+          id='boardsizeInput'
+          onChange={handleBoardsizeInputChange}
+        ></input>
+        <input type='submit' value='submit'></input>
+      </form>
+    </div>
+  );
 };
 
 export default App;
